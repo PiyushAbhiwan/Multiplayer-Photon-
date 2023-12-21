7ae4713927f0bae417795c1a2e6f28a3a069102c
piyush malviya <piyush.abhiwan@gmail.com>
2023-12-21 12:45:54 -0800

    both playerName show in both screen

--- Assets/PhotonScripts/PhotonManager.cs
+++ Assets/PhotonScripts/PhotonManager.cs
@@ -11,7 +11,7 @@ using UnityEngine.SceneManagement;
 public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks 
 {
     
-    #region Panels , InputFields and Buttons
+    #region Panels , InputFields, Text and Buttons
     public GameObject connectionPanel, lobbyPanel, roomPanel , roomListPanel;
     public Button connectBtn, createRoomBtn, JoinRoomBtn, joinBtn, startBtn, leaveBtn, backToLobbyBtn, leaveLobbyBtn;
     public TMP_Text clientstatetxt, playerName1, playerName2;
@@ -24,7 +24,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
 
     private Dictionary<string, RoomInfo> roomListData;
     private Dictionary<string, GameObject> roomListObjects;
-
+   
     #region Unity Methods
     public void Start()
     {
@@ -61,7 +61,8 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
 
 
     #region Static Data for event
-    public static byte scene = 100;
+    public static byte scene = 100, playerData =101;
+   
     #endregion
     #region UIMethods
 
@@ -131,7 +132,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
             startBtn.gameObject.SetActive(false);
         }*/
         PhotonNetwork.JoinRoom(roomName);
-        
     }
     public void ClearRoomList()
     {
@@ -152,8 +152,9 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
              PhotonNetwork.LocalPlayer,
              sceneName
         };
-        RaiseEvt(102,data,ReceiverGroup.Others);
+        RaiseEvt(scene,data,ReceiverGroup.Others);
         SceneManager.LoadScene(sceneName);
+        Debug.Log("RaiseEvt"+ scene);
     }
 
 
@@ -207,10 +208,23 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
         }
         else
         {
-            playerName2.text = PhotonNetwork.LocalPlayer.NickName;
+           //s playerName2.text = PhotonNetwork.LocalPlayer.NickName;
             roomPanel.SetActive(true);
             roomListPanel.SetActive(false);
             startBtn.gameObject.SetActive(false);
+
+            playerName1.text = PhotonNetwork.PlayerList[0].NickName;
+            playerName2.text = PhotonNetwork.PlayerList[1].NickName;
+            object[] data =
+            {
+                        PhotonNetwork.LocalPlayer,
+                        playerName1,
+                        playerName2
+                    };
+            Debug.Log("player1...>" + playerName1);
+            Debug.Log("player2...>" + playerName2);
+            Debug.Log("Event Raise " + playerData);
+            RaiseEvt(playerData, data, ReceiverGroup.All);
         }
     }
 
@@ -245,7 +259,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
         {
             GameObject roomListItemObject = Instantiate(roomListPrefab);
             roomListItemObject.transform.SetParent(roomListParent.transform);
-            
             // room name  , player, button
             roomListItemObject.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = roomItem.Name;
             roomListItemObject.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = roomItem.PlayerCount + "/ " + roomItem.MaxPlayers;
@@ -283,13 +296,9 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
        
       
         Debug.Log(newPlayer.NickName + "Joined");
-/*        if (!PhotonNetwork.IsMasterClient)
-        {
-            roomListPanel.SetActive(false);
-            roomPanel.SetActive(true);
-            startBtn.gameObject.SetActive(false);
-        }*/
-       
+        playerName1.text = PhotonNetwork.LocalPlayer.NickName;
+        playerName2.text = newPlayer.NickName;
+
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
@@ -305,12 +314,26 @@ public class PhotonManager : MonoBehaviourPunCallbacks , ILobbyCallbacks
 
     public void OnEvent(EventData photonEvent)
     {
+        
         Debug.Log(photonEvent.Code + "...EventCode");
-        if (photonEvent.Code.Equals(scene))
+       
+        if (photonEvent.Code == playerData)
         {
+            Debug.Log("Event Recieved " + playerData);
             var receivedData = (object[])photonEvent.CustomData;
             var player = (Player)receivedData[0];
-
+            var player1Name = (string)receivedData[1];
+            var player2Name = (string)receivedData[2];
+            playerName1.text = player1Name;
+            playerName2.text = player2Name;
+        }
+        if (photonEvent.Code == scene)
+        {
+            Debug.Log("Event Recieved for scene" + scene);
+            var receivedData = (object[])photonEvent.CustomData;
+            var player = (Player)receivedData[0];
+            SceneManager.LoadScene("GameScene");
+          
         }
     }
     #endregion
